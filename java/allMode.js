f7a7de4f5d3f2f55fcb4d1f9339852f9a86be645
HY-Joe <62640950+HY-Joe@users.noreply.github.com>
2020-08-20 00:09:12 +0900

    update

---
+++ java/allMode.js
@@ -0,0 +1,90 @@
+var keyName, chk, getData;
+var chk1, chk2;
+var comicNum;
+
+readMode();
+
+function readMode() {
+  keyName = "mode1";
+  funcGetItem(keyName);
+
+  chk = getData;
+  if (chk == null || chk == "undefined") funcSetItem(keyName, "panel");
+
+  funcGetItem(keyName);
+  chk = getData;
+
+  $("input:radio[name=readMode]:input[value=" + chk + "]").attr(
+    "checked",
+    true
+  );
+}
+
+function chAllComicMode(value) {
+  var xx = value;
+
+  funcSetItem("mode1", xx);
+
+  funcGetItem("mode1");
+
+  readMode();
+}
+
+function startComic() {
+  funcGetItem("comicNum");
+  comicNum = getData;
+
+  funcGetItem(keyName);
+  readingMode = getData;
+
+  var name = "mode1";
+  funcGetItem(name);
+  var modeName = getData;
+
+  if (comicNum == "1") {
+    if (modeName == "page") funcSetItem("page1", 1);
+    else if (modeName == "scrip") funcSetItem("scrip1", 1);
+    else if (modeName == "panel") funcSetItem("panel1", 1);
+  } else if (comicNum == "2") {
+    if (modeName == "page") funcSetItem("page2", 1);
+    else if (modeName == "scrip") funcSetItem("scrip2", 1);
+    else if (modeName == "panel") funcSetItem("panel2", 1);
+  }
+  location.href = readingMode + "_" + comicNum + ".html";
+}
+
+function continuesComic() {
+  funcGetItem("comicNum");
+  comicNum = getData;
+
+  var name = "mode1";
+  funcGetItem(name);
+  var modeName = getData;
+
+  if (comicNum == "1") {
+    if (modeName == "page") location.href = modeName + "_" + comicNum + ".html";
+    else if (modeName == "scrip")
+      location.href = modeName + "_" + comicNum + ".html";
+    else if (modeName == "panel")
+      location.href = modeName + "_" + comicNum + ".html";
+  } else if (comicNum == "2") {
+    if (modeName == "page") location.href = modeName + "_" + comicNum + ".html";
+    else if (modeName == "scrip")
+      location.href = modeName + "_" + comicNum + ".html";
+    else if (modeName == "panel")
+      location.href = modeName + "_" + comicNum + ".html";
+  }
+}
+
+function funcSetItem(key, value) {
+  this.key = key;
+  this.value = value;
+
+  sessionStorage.setItem(this.key, this.value);
+}
+
+function funcGetItem(key) {
+  this.key = key;
+
+  getData = sessionStorage.getItem(this.key);
+}
