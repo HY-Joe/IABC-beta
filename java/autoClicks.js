5747fe85c59709b4a9d2da60955199087fe344bc
HY-Joe <62640950+HY-Joe@users.noreply.github.com>
2020-08-25 20:35:17 +0900

    Update

---
+++ java/autoClicks.js
@@ -0,0 +1,18 @@
+// function autoClick() {
+// 	document.getElementById('panel').trigger("click");
+
+// var x = document.getElementById('panel').innerHTML;	
+// 	console.log(x);
+// }
+
+window.onload = function() {
+   // document.getElementById("play-panel").click();
+   fox_panel_1();
+   fox_panel_2();
+   fox_panel_3();
+   fox_panel_4();
+   fox_panel_5();
+   fox_panel_6();
+}
+
+// $('.panel').bind("click", function () { alert('bind()를 사용하여 페이지 로드시 버튼 클릭'); });
